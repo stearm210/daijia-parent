2221b7bf60d6e4f1af246e9b6c210254b23fdc18
stearm210 <pyzxWoo@163.com>
2025-03-06 16:07:55 +0800

    修改nacos地址

--- service/service-customer/src/main/java/com/atguigu/daijia/customer/config/WxConfigProperties.java
+++ service/service-customer/src/main/java/com/atguigu/daijia/customer/config/WxConfigProperties.java
@@ -4,8 +4,11 @@ import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
+//component代表这个类是一个配置类
 @Component
+//data为生成get、set方法
 @Data
+//对应nacos的配置文件
 @ConfigurationProperties(prefix = "wx.miniapp")
 public class WxConfigProperties {
     private String appId;
