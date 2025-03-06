2303687034aa61baca5e3c4e060beb143dde325e
stearm210 <pyzxWoo@163.com>
2025-03-06 16:17:30 +0800

    修改nacos地址

--- service/service-customer/src/main/java/com/atguigu/daijia/customer/config/WxConfigOperator.java
+++ service/service-customer/src/main/java/com/atguigu/daijia/customer/config/WxConfigOperator.java
@@ -14,6 +14,14 @@ public class WxConfigOperator {
     @Autowired
     private WxConfigProperties wxConfigProperties;
 
+     /*
+      * @Title: wxMaService
+      * @Author: pyzxW
+      * @Date: 2025-03-06 16:17:08
+      * @Params:
+      * @Return: null
+      * @Description: 专门用于针对微信小程序操作
+      */
     @Bean
     public WxMaService wxMaService() {
 
