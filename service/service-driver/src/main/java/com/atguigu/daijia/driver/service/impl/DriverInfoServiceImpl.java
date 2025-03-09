d3951a2d46422e0779264613bf8d5405f13700f2
stearm210 <pyzxWoo@163.com>
2025-03-09 16:20:03 +0800

    司机端登录操作

--- service/service-driver/src/main/java/com/atguigu/daijia/driver/service/impl/DriverInfoServiceImpl.java
+++ service/service-driver/src/main/java/com/atguigu/daijia/driver/service/impl/DriverInfoServiceImpl.java
@@ -70,49 +70,66 @@ public class DriverInfoServiceImpl extends ServiceImpl<DriverInfoMapper, DriverI
     public Long login(String code) {
         try {
             //根据code + 小程序id + 秘钥请求微信接口，返回openid
-            WxMaJscode2SessionResult sessionInfo =
-                    wxMaService.getUserService().getSessionInfo(code);
+            WxMaJscode2SessionResult sessionInfo = wxMaService.getUserService().getSessionInfo(code);
+            //获取openid
             String openid = sessionInfo.getOpenid();
 
-            //根据openid查询是否第一次登录
-            LambdaQueryWrapper<DriverInfo> wrapper = new LambdaQueryWrapper<>();
-            wrapper.eq(DriverInfo::getWxOpenId,openid);
-            DriverInfo driverInfo = driverInfoMapper.selectOne(wrapper);
-
-            if(driverInfo == null) {
-                //添加司机基本信息
-                driverInfo = new DriverInfo();
-                driverInfo.setNickname(String.valueOf(System.currentTimeMillis()));
-                driverInfo.setAvatarUrl("https://oss.aliyuncs.com/aliyun_id_photo_bucket/default_handsome.jpg");
-                driverInfo.setWxOpenId(openid);
-                driverInfoMapper.insert(driverInfo);
-
-                //初始化司机设置
-                DriverSet driverSet = new DriverSet();
-                driverSet.setDriverId(driverInfo.getId());
-                driverSet.setOrderDistance(new BigDecimal(0));//0：无限制
-                driverSet.setAcceptDistance(new BigDecimal(SystemConstant.ACCEPT_DISTANCE));//默认接单范围：5公里
-                driverSet.setIsAutoAccept(0);//0：否 1：是
-                driverSetMapper.insert(driverSet);
-
-                //初始化司机账户信息
-                DriverAccount driverAccount = new DriverAccount();
-                driverAccount.setDriverId(driverInfo.getId());
-                driverAccountMapper.insert(driverAccount);
-            }
-
-            //记录司机登录信息
-            DriverLoginLog driverLoginLog = new DriverLoginLog();
-            driverLoginLog.setDriverId(driverInfo.getId());
-            driverLoginLog.setMsg("小程序登录");
-            driverLoginLogMapper.insert(driverLoginLog);
-
-            //返回司机id
-            return driverInfo.getId();
         } catch (WxErrorException e) {
-            throw new GuiguException(ResultCodeEnum.DATA_ERROR);
+            throw new RuntimeException(e);
         }
+        return null;
     }
+
+
+//    @Override
+//    public Long login(String code) {
+//        try {
+//            //根据code + 小程序id + 秘钥请求微信接口，返回openid
+//            WxMaJscode2SessionResult sessionInfo =
+//                    wxMaService.getUserService().getSessionInfo(code);
+//            String openid = sessionInfo.getOpenid();
+//
+//            //根据openid查询是否第一次登录
+//            LambdaQueryWrapper<DriverInfo> wrapper = new LambdaQueryWrapper<>();
+//            wrapper.eq(DriverInfo::getWxOpenId,openid);
+//            DriverInfo driverInfo = driverInfoMapper.selectOne(wrapper);
+//
+//            if(driverInfo == null) {
+//                //添加司机基本信息
+//                driverInfo = new DriverInfo();
+//                driverInfo.setNickname(String.valueOf(System.currentTimeMillis()));
+//                driverInfo.setAvatarUrl("https://oss.aliyuncs.com/aliyun_id_photo_bucket/default_handsome.jpg");
+//                driverInfo.setWxOpenId(openid);
+//                driverInfoMapper.insert(driverInfo);
+//
+//                //初始化司机设置
+//                DriverSet driverSet = new DriverSet();
+//                driverSet.setDriverId(driverInfo.getId());
+//                driverSet.setOrderDistance(new BigDecimal(0));//0：无限制
+//                driverSet.setAcceptDistance(new BigDecimal(SystemConstant.ACCEPT_DISTANCE));//默认接单范围：5公里
+//                driverSet.setIsAutoAccept(0);//0：否 1：是
+//                driverSetMapper.insert(driverSet);
+//
+//                //初始化司机账户信息
+//                DriverAccount driverAccount = new DriverAccount();
+//                driverAccount.setDriverId(driverInfo.getId());
+//                driverAccountMapper.insert(driverAccount);
+//            }
+//
+//            //记录司机登录信息
+//            DriverLoginLog driverLoginLog = new DriverLoginLog();
+//            driverLoginLog.setDriverId(driverInfo.getId());
+//            driverLoginLog.setMsg("小程序登录");
+//            driverLoginLogMapper.insert(driverLoginLog);
+//
+//            //返回司机id
+//            return driverInfo.getId();
+//        } catch (WxErrorException e) {
+//            throw new GuiguException(ResultCodeEnum.DATA_ERROR);
+//        }
+//    }
+
+
 
     //获取司机登录信息
     @Override
