25003522c6e0b922eda55850ee68bf4a936a6b70
stearm210 <pyzxWoo@163.com>
2025-03-11 14:41:46 +0800

    司机端登录操作

--- service/service-driver/src/main/java/com/atguigu/daijia/driver/service/impl/CosServiceImpl.java
+++ service/service-driver/src/main/java/com/atguigu/daijia/driver/service/impl/CosServiceImpl.java
@@ -45,54 +45,102 @@ public class CosServiceImpl implements CosService {
       * @Return: null
       * @Description: 腾讯云上传操作
       */
-    @Override
-    public CosUploadVo upload(MultipartFile file, String path) {
-        //获取cosClient对象
-        COSClient cosClient = this.getCosClient();
-        //文件上传
-        //元数据信息
-        ObjectMetadata meta = new ObjectMetadata();
-        meta.setContentLength(file.getSize());
-        meta.setContentEncoding("UTF-8");
-        meta.setContentType(file.getContentType());
+     @Override
+     public CosUploadVo upload(MultipartFile file, String path) {
+         // 1 初始化用户身份信息（secretId, secretKey）。
+         String secretId = tencentCloudProperties.getSecretId();
+         String secretKey = tencentCloudProperties.getSecretKey();
+         COSCredentials cred = new BasicCOSCredentials(secretId, secretKey);
+         // 2 设置 bucket 的地域, COS 地域
+         Region region = new Region(tencentCloudProperties.getRegion());
+         ClientConfig clientConfig = new ClientConfig(region);
+         // 这里建议设置使用 https 协议
+         clientConfig.setHttpProtocol(HttpProtocol.https);
+         // 3 生成 cos 客户端。
+         COSClient cosClient = new COSClient(cred, clientConfig);
 
-        //向存储桶中保存文件
-        String fileType = file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf(".")); //文件后缀名
-        String uploadPath = "/driver/" + path + "/" + UUID.randomUUID().toString().replaceAll("-", "") + fileType;
-        // 01.jpg
-        //这里生成的是随机的文件路径
-        // /driver/auth/0o98754.jpg
-        PutObjectRequest putObjectRequest = null;
-        try {
-            //1 bucket名称
-            //2
-            putObjectRequest = new PutObjectRequest(tencentCloudProperties.getBucketPrivate(),
-                    uploadPath,
-                    file.getInputStream(),
-                    meta);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        putObjectRequest.setStorageClass(StorageClass.Standard);
-        PutObjectResult putObjectResult = cosClient.putObject(putObjectRequest); //上传文件
-        cosClient.shutdown();
+         //文件上传
+         //元数据信息
+         ObjectMetadata meta = new ObjectMetadata();
+         meta.setContentLength(file.getSize());
+         meta.setContentEncoding("UTF-8");
+         meta.setContentType(file.getContentType());
 
-        //图片审核
-        Boolean imageAuditing = ciService.imageAuditing(uploadPath);
-        if(!imageAuditing) {
-            //删除违规图片
-            cosClient.deleteObject(tencentCloudProperties.getBucketPrivate(),uploadPath);
-            throw new GuiguException(ResultCodeEnum.IMAGE_AUDITION_FAIL);
-        }
+         //向存储桶中保存文件
+         String fileType = file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf(".")); //文件后缀名
+         String uploadPath = "/driver/" + path + "/" + UUID.randomUUID().toString().replaceAll("-", "") + fileType;
+         // 01.jpg
+         // /driver/auth/0o98754.jpg
+         PutObjectRequest putObjectRequest = null;
+         try {
+             //1 bucket名称
+             //2
+             putObjectRequest = new PutObjectRequest(tencentCloudProperties.getBucketPrivate(),
+                     uploadPath,
+                     file.getInputStream(),
+                     meta);
+         } catch (IOException e) {
+             throw new RuntimeException(e);
+         }
+         putObjectRequest.setStorageClass(StorageClass.Standard);
+         PutObjectResult putObjectResult = cosClient.putObject(putObjectRequest); //上传文件
+         cosClient.shutdown();
 
-        //返回vo对象
-        CosUploadVo cosUploadVo = new CosUploadVo();
-        cosUploadVo.setUrl(uploadPath);
-        //图片临时访问url，回显使用
-        String imageUrl = this.getImageUrl(uploadPath);
-        cosUploadVo.setShowUrl(imageUrl);
-        return cosUploadVo;
-    }
+         //返回vo对象
+         CosUploadVo cosUploadVo = new CosUploadVo();
+         cosUploadVo.setUrl(uploadPath);
+         //TODO 图片临时访问url，回显使用
+         cosUploadVo.setShowUrl("");
+         return cosUploadVo;
+     }
+//    @Override
+//    public CosUploadVo upload(MultipartFile file, String path) {
+//        //获取cosClient对象
+//        COSClient cosClient = this.getCosClient();
+//        //文件上传
+//        //元数据信息
+//        ObjectMetadata meta = new ObjectMetadata();
+//        meta.setContentLength(file.getSize());
+//        meta.setContentEncoding("UTF-8");
+//        meta.setContentType(file.getContentType());
+//
+//        //向存储桶中保存文件
+//        String fileType = file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf(".")); //文件后缀名
+//        String uploadPath = "/driver/" + path + "/" + UUID.randomUUID().toString().replaceAll("-", "") + fileType;
+//        // 01.jpg
+//        //这里生成的是随机的文件路径
+//        // /driver/auth/0o98754.jpg
+//        PutObjectRequest putObjectRequest = null;
+//        try {
+//            //1 bucket名称
+//            //2
+//            putObjectRequest = new PutObjectRequest(tencentCloudProperties.getBucketPrivate(),
+//                    uploadPath,
+//                    file.getInputStream(),
+//                    meta);
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
+//        putObjectRequest.setStorageClass(StorageClass.Standard);
+//        PutObjectResult putObjectResult = cosClient.putObject(putObjectRequest); //上传文件
+//        cosClient.shutdown();
+//
+//        //图片审核
+//        Boolean imageAuditing = ciService.imageAuditing(uploadPath);
+//        if(!imageAuditing) {
+//            //删除违规图片
+//            cosClient.deleteObject(tencentCloudProperties.getBucketPrivate(),uploadPath);
+//            throw new GuiguException(ResultCodeEnum.IMAGE_AUDITION_FAIL);
+//        }
+//
+//        //返回vo对象
+//        CosUploadVo cosUploadVo = new CosUploadVo();
+//        cosUploadVo.setUrl(uploadPath);
+//        //图片临时访问url，回显使用
+//        String imageUrl = this.getImageUrl(uploadPath);
+//        cosUploadVo.setShowUrl(imageUrl);
+//        return cosUploadVo;
+//    }
 
     public COSClient getCosClient() {
         String secretId = tencentCloudProperties.getSecretId();
