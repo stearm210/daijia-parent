e21a1e7ce94d044fe245d163ee4591cf5977ace2
stearm210 <pyzxWoo@163.com>
2025-03-17 15:21:23 +0800

    创建司机人脸信息

--- service/service-driver/src/main/java/com/atguigu/daijia/driver/service/impl/DriverInfoServiceImpl.java
+++ service/service-driver/src/main/java/com/atguigu/daijia/driver/service/impl/DriverInfoServiceImpl.java
@@ -259,6 +259,14 @@ public class DriverInfoServiceImpl extends ServiceImpl<DriverInfoMapper, DriverI
     }
 
     //创建司机人脸模型
+     /*
+      * @Title: creatDriverFaceModel
+      * @Author: pyzxW
+      * @Date: 2025-03-17 15:18:15
+      * @Params: [driverFaceModelForm]
+      * @Return: Boolean
+      * @Description: 创建司机人脸模型
+      */
     @Override
     public Boolean creatDriverFaceModel(DriverFaceModelForm driverFaceModelForm) {
         //根据司机id获取司机信息
@@ -269,6 +277,8 @@ public class DriverInfoServiceImpl extends ServiceImpl<DriverInfoMapper, DriverI
             // 实例化一个认证对象，入参需要传入腾讯云账户 SecretId 和 SecretKey，此处还需注意密钥对的保密
             // 代码泄露可能会导致 SecretId 和 SecretKey 泄露，并威胁账号下所有资源的安全性。以下代码示例仅供参考，建议采用更安全的方式来使用密钥，请参见：https://cloud.tencent.com/document/product/1278/85305
             // 密钥可前往官网控制台 https://console.cloud.tencent.com/cam/capi 进行获取
+
+            //获取对应的ID和秘钥
             Credential cred = new Credential(tencentCloudProperties.getSecretId(),
                                             tencentCloudProperties.getSecretKey());
             // 实例化一个http选项，可选的，没有特殊需求可以跳过
@@ -278,6 +288,7 @@ public class DriverInfoServiceImpl extends ServiceImpl<DriverInfoMapper, DriverI
             ClientProfile clientProfile = new ClientProfile();
             clientProfile.setHttpProfile(httpProfile);
             // 实例化要请求产品的client对象,clientProfile是可选的
+            //传入地域
             IaiClient client = new IaiClient(cred, tencentCloudProperties.getRegion(),
                                                      clientProfile);
             // 实例化一个请求对象,每个接口都会对应一个request对象
