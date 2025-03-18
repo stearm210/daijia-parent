7923dbbec3cb563eeb277dd07cf8ba4cf08d551d
stearm210 <pyzxWoo@163.com>
2025-03-18 16:01:46 +0800

    计算驾驶线路

--- service/service-map/src/main/java/com/atguigu/daijia/map/service/impl/MapServiceImpl.java
+++ service/service-map/src/main/java/com/atguigu/daijia/map/service/impl/MapServiceImpl.java
@@ -29,33 +29,41 @@ public class MapServiceImpl implements MapService {
     private String key;
 
     //计算驾驶线路
+     /*
+      * @Title: calculateDrivingLine
+      * @Author: pyzxW
+      * @Date: 2025-03-18 15:38:40
+      * @Params: [calculateDrivingLineForm]
+      * @Return: DrivingLineVo
+      * @Description: 计算驾驶路线
+      */
     @Override
     public DrivingLineVo calculateDrivingLine(CalculateDrivingLineForm calculateDrivingLineForm) {
-        //请求腾讯提供接口，按照接口要求传递相关参数，返回需要结果
+        //请求腾讯提供的接口，按照接口要求传递相关参数，返回需要结果
         //使用HttpClient，目前Spring封装调用工具使用RestTemplate
         //定义调用腾讯地址
         String url = "https://apis.map.qq.com/ws/direction/v1/driving/?from={from}&to={to}&key={key}";
 
         //封装传递参数
         Map<String,String> map = new HashMap();
-        //开始位置
-        // 经纬度：比如 北纬40 东经116
+        //开始之位置：北纬40 东京116
         map.put("from",calculateDrivingLineForm.getStartPointLatitude()+","+calculateDrivingLineForm.getStartPointLongitude());
         //结束位置
         map.put("to",calculateDrivingLineForm.getEndPointLatitude()+","+calculateDrivingLineForm.getEndPointLongitude());
-        //key
+        //封装自己的key值
         map.put("key",key);
 
-        //使用RestTemplate调用 GET
+        //使用RestTemplate调用 GET 请求
         JSONObject result = restTemplate.getForObject(url, JSONObject.class, map);
+
         //处理返回结果
         //判断调用是否成功
         int status = result.getIntValue("status");
         if(status != 0) {//失败
             throw new GuiguException(ResultCodeEnum.MAP_FAIL);
         }
-
         //获取返回路线信息
+        //返回result下的数组routes中的信息，也就是路线
         JSONObject route =
                 result.getJSONObject("result").getJSONArray("routes").getJSONObject(0);
 
@@ -65,11 +73,55 @@ public class MapServiceImpl implements MapService {
         drivingLineVo.setDuration(route.getBigDecimal("duration"));
         //距离  6.583 == 6.58 / 6.59
         drivingLineVo.setDistance(route.getBigDecimal("distance")
-                 .divide(new BigDecimal(1000))
+                .divide(new BigDecimal(1000))
                 .setScale(2, RoundingMode.HALF_UP));
         //路线
         drivingLineVo.setPolyline(route.getJSONArray("polyline"));
-
         return drivingLineVo;
     }
+
+
+//    @Override
+//    public DrivingLineVo calculateDrivingLine(CalculateDrivingLineForm calculateDrivingLineForm) {
+//        //请求腾讯提供接口，按照接口要求传递相关参数，返回需要结果
+//        //使用HttpClient，目前Spring封装调用工具使用RestTemplate
+//        //定义调用腾讯地址
+//        String url = "https://apis.map.qq.com/ws/direction/v1/driving/?from={from}&to={to}&key={key}";
+//
+//        //封装传递参数
+//        Map<String,String> map = new HashMap();
+//        //开始位置
+//        // 经纬度：比如 北纬40 东经116
+//        map.put("from",calculateDrivingLineForm.getStartPointLatitude()+","+calculateDrivingLineForm.getStartPointLongitude());
+//        //结束位置
+//        map.put("to",calculateDrivingLineForm.getEndPointLatitude()+","+calculateDrivingLineForm.getEndPointLongitude());
+//        //key
+//        map.put("key",key);
+//
+//        //使用RestTemplate调用 GET
+//        JSONObject result = restTemplate.getForObject(url, JSONObject.class, map);
+//        //处理返回结果
+//        //判断调用是否成功
+//        int status = result.getIntValue("status");
+//        if(status != 0) {//失败
+//            throw new GuiguException(ResultCodeEnum.MAP_FAIL);
+//        }
+//
+//        //获取返回路线信息
+//        JSONObject route =
+//                result.getJSONObject("result").getJSONArray("routes").getJSONObject(0);
+//
+//        //创建vo对象
+//        DrivingLineVo drivingLineVo = new DrivingLineVo();
+//        //预估时间
+//        drivingLineVo.setDuration(route.getBigDecimal("duration"));
+//        //距离  6.583 == 6.58 / 6.59
+//        drivingLineVo.setDistance(route.getBigDecimal("distance")
+//                 .divide(new BigDecimal(1000))
+//                .setScale(2, RoundingMode.HALF_UP));
+//        //路线
+//        drivingLineVo.setPolyline(route.getJSONArray("polyline"));
+//
+//        return drivingLineVo;
+//    }
 }
