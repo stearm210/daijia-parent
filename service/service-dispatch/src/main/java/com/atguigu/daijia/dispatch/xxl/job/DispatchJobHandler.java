943e0e505910aca5fbdc2e3925d2c3000ae9e7e1
stearm210 <pyzxWoo@163.com>
2025-03-27 20:01:03 +0800

    计算驾驶线路

--- service/service-dispatch/src/main/java/com/atguigu/daijia/dispatch/xxl/job/DispatchJobHandler.java
+++ service/service-dispatch/src/main/java/com/atguigu/daijia/dispatch/xxl/job/DispatchJobHandler.java
@@ -1,14 +1,16 @@
 package com.atguigu.daijia.dispatch.xxl.job;
 
-
 import com.xxl.job.core.handler.annotation.XxlJob;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
+
+@Slf4j
 @Component
 public class DispatchJobHandler {
 
     @XxlJob("firstJobHandler")
-    public void testJobHandler() {
-        System.out.println("xxl-job项目集成测试");
+    public void firstJobHandler() {
+        log.info("xxl-job项目集成测试");
     }
 }
