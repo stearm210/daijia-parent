1192bf71c6a1d46e88e0f28637dd1055d4ffe099
stearm210 <pyzxWoo@163.com>
2025-03-30 15:33:12 +0800

    任务之日志信息

--- service/service-dispatch/src/main/java/com/atguigu/daijia/dispatch/service/impl/NewOrderServiceImpl.java
+++ service/service-dispatch/src/main/java/com/atguigu/daijia/dispatch/service/impl/NewOrderServiceImpl.java
@@ -118,77 +118,96 @@ public class NewOrderServiceImpl implements NewOrderService {
 //        return orderJob.getJobId();
 //    }
 
+    /*
+    * 只有在订单被指派任务之后，才能搜索附近的代驾司机
+    * */
     //执行任务：搜索附近代驾司机
     @Override
     public void executeTask(long jobId) {
-        //1 根据jobid查询数据库，当前任务是否已经创建
+        //1.根据jobid查询数据库，当前任务是否已经创建
         //如果没有创建，不往下执行了
-        LambdaQueryWrapper<OrderJob> wrapper = new LambdaQueryWrapper<>();
-        wrapper.eq(OrderJob::getJobId,jobId);
-        OrderJob orderJob = orderJobMapper.selectOne(wrapper);
-        if(orderJob == null) {
-            //不往下执行了
-            return;
-        }
 
-        //2 查询订单状态，如果当前订单接单状态，继续执行。如果当前订单不是接单状态，停止任务调度
-        //获取OrderJob里面对象
-        String jsonString = orderJob.getParameter();
-        NewOrderTaskVo newOrderTaskVo = JSONObject.parseObject(jsonString, NewOrderTaskVo.class);
+        //2.查询订单状态，如果当前订单接单状态，继续执行。如果当前订单不是接单状态，停止任务调度
 
-        //获取orderId
-        Long orderId = newOrderTaskVo.getOrderId();
-        Integer status = orderInfoFeignClient.getOrderStatus(orderId).getData();
-        if(status.intValue() != OrderStatus.WAITING_ACCEPT.getStatus().intValue()) {
-            //停止任务调度
-            xxlJobClient.stopJob(jobId);
-            return;
-        }
+        //3.远程调用：搜索附近满足条件可以接单的司机
 
-        //3 远程调用:搜索附近满足条件可以接单司机
-        //4 远程调用之后，获取满足可以接单司机集合
-        SearchNearByDriverForm searchNearByDriverForm = new SearchNearByDriverForm();
-        searchNearByDriverForm.setLongitude(newOrderTaskVo.getStartPointLongitude());
-        searchNearByDriverForm.setLatitude(newOrderTaskVo.getStartPointLatitude());
-        searchNearByDriverForm.setMileageDistance(newOrderTaskVo.getExpectDistance());
-        //远程调用
-        List<NearByDriverVo> nearByDriverVoList =
-                locationFeignClient.searchNearByDriver(searchNearByDriverForm).getData();
+        //4.远程调用之后，获取满足可以接单的司机集合
 
-        //5 遍历司机集合，得到每个司机，为每个司机创建临时队列，存储新订单信息
-        nearByDriverVoList.forEach(driver -> {
-            //使用Redis的set类型
-            //根据订单id生成key
-            String repeatKey =
-                    RedisConstant.DRIVER_ORDER_REPEAT_LIST+newOrderTaskVo.getOrderId();
-            //记录司机id，防止重复推送
-            Boolean isMember = redisTemplate.opsForSet().isMember(repeatKey, driver.getDriverId());
-            if(!isMember) {
-                //把订单信息推送给满足条件多个司机
-                redisTemplate.opsForSet().add(repeatKey,driver.getDriverId());
-                //过期时间：15分钟，超过15分钟没有接单自动取消
-                redisTemplate.expire(repeatKey,
-                        RedisConstant.DRIVER_ORDER_REPEAT_LIST_EXPIRES_TIME,
-                        TimeUnit.MINUTES);
+        //5.遍历司机集合，得到每个司机，为每个司机，创建一个临时队列，存储新订单信息
 
-                NewOrderDataVo newOrderDataVo = new NewOrderDataVo();
-                newOrderDataVo.setOrderId(newOrderTaskVo.getOrderId());
-                newOrderDataVo.setStartLocation(newOrderTaskVo.getStartLocation());
-                newOrderDataVo.setEndLocation(newOrderTaskVo.getEndLocation());
-                newOrderDataVo.setExpectAmount(newOrderTaskVo.getExpectAmount());
-                newOrderDataVo.setExpectDistance(newOrderTaskVo.getExpectDistance());
-                newOrderDataVo.setExpectTime(newOrderTaskVo.getExpectTime());
-                newOrderDataVo.setFavourFee(newOrderTaskVo.getFavourFee());
-                newOrderDataVo.setDistance(driver.getDistance());
-                newOrderDataVo.setCreateTime(newOrderTaskVo.getCreateTime());
-                //新订单保存司机的临时队列，Redis里面List集合
-                String key = RedisConstant.DRIVER_ORDER_TEMP_LIST+driver.getDriverId();
-                redisTemplate.opsForList().leftPush(key,JSONObject.toJSONString(newOrderDataVo));
-                //过期时间：1分钟
-                redisTemplate.expire(key,1, TimeUnit.MINUTES);
-            }
-        });
+
     }
+
+//    @Override
+//    public void executeTask(long jobId) {
+//        //1 根据jobid查询数据库，当前任务是否已经创建
+//        //如果没有创建，不往下执行了
+//        LambdaQueryWrapper<OrderJob> wrapper = new LambdaQueryWrapper<>();
+//        wrapper.eq(OrderJob::getJobId,jobId);
+//        OrderJob orderJob = orderJobMapper.selectOne(wrapper);
+//        if(orderJob == null) {
+//            //不往下执行了
+//            return;
+//        }
+//
+//        //2 查询订单状态，如果当前订单接单状态，继续执行。如果当前订单不是接单状态，停止任务调度
+//        //获取OrderJob里面对象
+//        String jsonString = orderJob.getParameter();
+//        NewOrderTaskVo newOrderTaskVo = JSONObject.parseObject(jsonString, NewOrderTaskVo.class);
+//
+//        //获取orderId
+//        Long orderId = newOrderTaskVo.getOrderId();
+//        Integer status = orderInfoFeignClient.getOrderStatus(orderId).getData();
+//        if(status.intValue() != OrderStatus.WAITING_ACCEPT.getStatus().intValue()) {
+//            //停止任务调度
+//            xxlJobClient.stopJob(jobId);
+//            return;
+//        }
+//
+//        //3 远程调用:搜索附近满足条件可以接单司机
+//        //4 远程调用之后，获取满足可以接单司机集合
+//        SearchNearByDriverForm searchNearByDriverForm = new SearchNearByDriverForm();
+//        searchNearByDriverForm.setLongitude(newOrderTaskVo.getStartPointLongitude());
+//        searchNearByDriverForm.setLatitude(newOrderTaskVo.getStartPointLatitude());
+//        searchNearByDriverForm.setMileageDistance(newOrderTaskVo.getExpectDistance());
+//        //远程调用
+//        List<NearByDriverVo> nearByDriverVoList =
+//                locationFeignClient.searchNearByDriver(searchNearByDriverForm).getData();
+//
+//        //5 遍历司机集合，得到每个司机，为每个司机创建临时队列，存储新订单信息
+//        nearByDriverVoList.forEach(driver -> {
+//            //使用Redis的set类型
+//            //根据订单id生成key
+//            String repeatKey =
+//                    RedisConstant.DRIVER_ORDER_REPEAT_LIST+newOrderTaskVo.getOrderId();
+//            //记录司机id，防止重复推送
+//            Boolean isMember = redisTemplate.opsForSet().isMember(repeatKey, driver.getDriverId());
+//            if(!isMember) {
+//                //把订单信息推送给满足条件多个司机
+//                redisTemplate.opsForSet().add(repeatKey,driver.getDriverId());
+//                //过期时间：15分钟，超过15分钟没有接单自动取消
+//                redisTemplate.expire(repeatKey,
+//                        RedisConstant.DRIVER_ORDER_REPEAT_LIST_EXPIRES_TIME,
+//                        TimeUnit.MINUTES);
+//
+//                NewOrderDataVo newOrderDataVo = new NewOrderDataVo();
+//                newOrderDataVo.setOrderId(newOrderTaskVo.getOrderId());
+//                newOrderDataVo.setStartLocation(newOrderTaskVo.getStartLocation());
+//                newOrderDataVo.setEndLocation(newOrderTaskVo.getEndLocation());
+//                newOrderDataVo.setExpectAmount(newOrderTaskVo.getExpectAmount());
+//                newOrderDataVo.setExpectDistance(newOrderTaskVo.getExpectDistance());
+//                newOrderDataVo.setExpectTime(newOrderTaskVo.getExpectTime());
+//                newOrderDataVo.setFavourFee(newOrderTaskVo.getFavourFee());
+//                newOrderDataVo.setDistance(driver.getDistance());
+//                newOrderDataVo.setCreateTime(newOrderTaskVo.getCreateTime());
+//                //新订单保存司机的临时队列，Redis里面List集合
+//                String key = RedisConstant.DRIVER_ORDER_TEMP_LIST+driver.getDriverId();
+//                redisTemplate.opsForList().leftPush(key,JSONObject.toJSONString(newOrderDataVo));
+//                //过期时间：1分钟
+//                redisTemplate.expire(key,1, TimeUnit.MINUTES);
+//            }
+//        });
+//    }
 
     //获取最新订单
     @Override
