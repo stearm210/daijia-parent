acdb40df47c3ac490af7a1992a77300665df8bd7
stearm210 <pyzxWoo@163.com>
2025-03-30 14:34:30 +0800

    任务调度

--- service/service-dispatch/src/main/java/com/atguigu/daijia/dispatch/service/impl/NewOrderServiceImpl.java
+++ service/service-dispatch/src/main/java/com/atguigu/daijia/dispatch/service/impl/NewOrderServiceImpl.java
@@ -54,32 +54,39 @@ public class NewOrderServiceImpl implements NewOrderService {
       */
     @Override
     public Long addAndStartTask(NewOrderTaskVo newOrderTaskVo) {
-        //1 判断当前订单是否启动任务调度
-        //根据订单id查询
-        LambdaQueryWrapper<OrderJob> wrapper = new LambdaQueryWrapper<>();
-        wrapper.eq(OrderJob::getOrderId,newOrderTaskVo.getOrderId());
-        OrderJob orderJob = orderJobMapper.selectOne(wrapper);
+        //1.判断当前订单是否启动任务调度
 
-        //2 没有启动，进行操作
-        if(orderJob == null) {
-            //创建并启动任务调度
-            //String executorHandler 执行任务job方法
-            // String param
-            // String corn 执行cron表达式
-            // String desc 描述信息
-            Long jobId = xxlJobClient.addAndStart("newOrderTaskHandler", "",
-                    "0 0/1 * * * ?",
-                    "新创建订单任务调度：" + newOrderTaskVo.getOrderId());
-
-            //记录任务调度信息
-            orderJob = new OrderJob();
-            orderJob.setOrderId(newOrderTaskVo.getOrderId());
-            orderJob.setJobId(jobId);
-            orderJob.setParameter(JSONObject.toJSONString(newOrderTaskVo));
-            orderJobMapper.insert(orderJob);
-        }
-        return orderJob.getJobId();
+        return null;
     }
+
+//    @Override
+//    public Long addAndStartTask(NewOrderTaskVo newOrderTaskVo) {
+//        //1 判断当前订单是否启动任务调度
+//        //根据订单id查询
+//        LambdaQueryWrapper<OrderJob> wrapper = new LambdaQueryWrapper<>();
+//        wrapper.eq(OrderJob::getOrderId,newOrderTaskVo.getOrderId());
+//        OrderJob orderJob = orderJobMapper.selectOne(wrapper);
+//
+//        //2 没有启动，进行操作
+//        if(orderJob == null) {
+//            //创建并启动任务调度
+//            //String executorHandler 执行任务job方法
+//            // String param
+//            // String corn 执行cron表达式
+//            // String desc 描述信息
+//            Long jobId = xxlJobClient.addAndStart("newOrderTaskHandler", "",
+//                    "0 0/1 * * * ?",
+//                    "新创建订单任务调度：" + newOrderTaskVo.getOrderId());
+//
+//            //记录任务调度信息
+//            orderJob = new OrderJob();
+//            orderJob.setOrderId(newOrderTaskVo.getOrderId());
+//            orderJob.setJobId(jobId);
+//            orderJob.setParameter(JSONObject.toJSONString(newOrderTaskVo));
+//            orderJobMapper.insert(orderJob);
+//        }
+//        return orderJob.getJobId();
+//    }
 
     //执行任务：搜索附近代驾司机
     @Override
