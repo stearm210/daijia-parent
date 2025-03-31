728f6ede90bc25689890e5706b617893dbfcad96
stearm210 <pyzxWoo@163.com>
2025-03-31 15:15:53 +0800

    任务之日志信息

--- service/service-driver/src/main/java/com/atguigu/daijia/driver/service/impl/DriverInfoServiceImpl.java
+++ service/service-driver/src/main/java/com/atguigu/daijia/driver/service/impl/DriverInfoServiceImpl.java
@@ -436,6 +436,47 @@ public class DriverInfoServiceImpl extends ServiceImpl<DriverInfoMapper, DriverI
         throw new GuiguException(ResultCodeEnum.DATA_ERROR);
     }
 
+    /*
+     * @Title: detectLiveFace
+     * @Author: pyzxW
+     * @Date: 2025-03-31 15:15:07
+     * @Params:
+     * @Return: null
+     * @Description: 人脸静态活体检测
+     */
+    //人脸静态活体检测
+    private Boolean detectLiveFace(String imageBase64) {
+        try{
+            // 实例化一个认证对象，入参需要传入腾讯云账户 SecretId 和 SecretKey，此处还需注意密钥对的保密
+            // 代码泄露可能会导致 SecretId 和 SecretKey 泄露，并威胁账号下所有资源的安全性。以下代码示例仅供参考，建议采用更安全的方式来使用密钥，请参见：https://cloud.tencent.com/document/product/1278/85305
+            // 密钥可前往官网控制台 https://console.cloud.tencent.com/cam/capi 进行获取
+            Credential cred = new Credential(tencentCloudProperties.getSecretId(),
+                    tencentCloudProperties.getSecretKey());
+            // 实例化一个http选项，可选的，没有特殊需求可以跳过
+            HttpProfile httpProfile = new HttpProfile();
+            httpProfile.setEndpoint("iai.tencentcloudapi.com");
+            // 实例化一个client选项，可选的，没有特殊需求可以跳过
+            ClientProfile clientProfile = new ClientProfile();
+            clientProfile.setHttpProfile(httpProfile);
+            // 实例化要请求产品的client对象,clientProfile是可选的
+            IaiClient client = new IaiClient(cred, tencentCloudProperties.getRegion(),
+                    clientProfile);
+            // 实例化一个请求对象,每个接口都会对应一个request对象
+            DetectLiveFaceRequest req = new DetectLiveFaceRequest();
+            req.setImage(imageBase64);
+            // 返回的resp是一个DetectLiveFaceResponse的实例，与请求对象对应
+            DetectLiveFaceResponse resp = client.DetectLiveFace(req);
+            // 输出json格式的字符串回包
+            System.out.println(DetectLiveFaceResponse.toJsonString(resp));
+            if(resp.getIsLiveness()) {
+                return true;
+            }
+        } catch (TencentCloudSDKException e) {
+            System.out.println(e.toString());
+        }
+        return false;
+    }
+
     //更新接单状态
     // update driver_set set status=? where driver_id=?
     @Override
@@ -476,36 +517,4 @@ public class DriverInfoServiceImpl extends ServiceImpl<DriverInfoMapper, DriverI
         return driverInfo.getWxOpenId();
     }
 
-    //人脸静态活体检测
-    private Boolean detectLiveFace(String imageBase64) {
-        try{
-            // 实例化一个认证对象，入参需要传入腾讯云账户 SecretId 和 SecretKey，此处还需注意密钥对的保密
-            // 代码泄露可能会导致 SecretId 和 SecretKey 泄露，并威胁账号下所有资源的安全性。以下代码示例仅供参考，建议采用更安全的方式来使用密钥，请参见：https://cloud.tencent.com/document/product/1278/85305
-            // 密钥可前往官网控制台 https://console.cloud.tencent.com/cam/capi 进行获取
-            Credential cred = new Credential(tencentCloudProperties.getSecretId(),
-                    tencentCloudProperties.getSecretKey());
-            // 实例化一个http选项，可选的，没有特殊需求可以跳过
-            HttpProfile httpProfile = new HttpProfile();
-            httpProfile.setEndpoint("iai.tencentcloudapi.com");
-            // 实例化一个client选项，可选的，没有特殊需求可以跳过
-            ClientProfile clientProfile = new ClientProfile();
-            clientProfile.setHttpProfile(httpProfile);
-            // 实例化要请求产品的client对象,clientProfile是可选的
-            IaiClient client = new IaiClient(cred, tencentCloudProperties.getRegion(),
-                    clientProfile);
-            // 实例化一个请求对象,每个接口都会对应一个request对象
-            DetectLiveFaceRequest req = new DetectLiveFaceRequest();
-            req.setImage(imageBase64);
-            // 返回的resp是一个DetectLiveFaceResponse的实例，与请求对象对应
-            DetectLiveFaceResponse resp = client.DetectLiveFace(req);
-            // 输出json格式的字符串回包
-            System.out.println(DetectLiveFaceResponse.toJsonString(resp));
-            if(resp.getIsLiveness()) {
-                return true;
-            }
-        } catch (TencentCloudSDKException e) {
-            System.out.println(e.toString());
-        }
-        return false;
-    }
 }
