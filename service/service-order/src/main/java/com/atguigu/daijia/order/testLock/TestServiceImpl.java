e4e1103ef8911a7a145287a4924b16e363173c6a
stearm210 <pyzxWoo@163.com>
2025-04-09 14:36:53 +0800

    分布式锁

--- service/service-order/src/main/java/com/atguigu/daijia/order/testLock/TestServiceImpl.java
+++ service/service-order/src/main/java/com/atguigu/daijia/order/testLock/TestServiceImpl.java
@@ -22,80 +22,80 @@ public class TestServiceImpl implements TestService{
     @Autowired
     private RedissonClient redissonClient;
 
-//    //Redisson实现
-//    @Override
-//    public void testLock()  {
-//
-//        //1 通过redisson创建锁对象
-//        RLock lock = redissonClient.getLock("lock1");
-//
-//        //2 尝试获取锁
-//        //(1) 阻塞一直等待直到获取到，获取锁之后，默认过期时间30s
-//        lock.lock();
-//
-//        //(2) 获取到锁，锁过期时间10s
-//       // lock.lock(10,TimeUnit.SECONDS);
-//
-//        //(3) 第一个参数获取锁等待时间
-//        //    第二个参数获取到锁，锁过期时间
-//        //        try {
-//        //            // true
-//        //            boolean tryLock = lock.tryLock(30, 10, TimeUnit.SECONDS);
-//        //        } catch (InterruptedException e) {
-//        //            throw new RuntimeException(e);
-//        //        }
-//
-//        //3 编写业务代码
-//        //1.先从redis中通过key num获取值  key提前手动设置 num 初始值：0
-//        String value = redisTemplate.opsForValue().get("num");
-//        //2.如果值为空则非法直接返回即可
-//        if (StringUtils.isBlank(value)) {
-//            return;
-//        }
-//        //3.对num值进行自增加一
-//        int num = Integer.parseInt(value);
-//        redisTemplate.opsForValue().set("num", String.valueOf(++num));
-//
-//        //4 释放锁
-//        lock.unlock();
-//    }
-
-
-    //redis里面setnx实现
+    //Redisson实现
     @Override
-    public synchronized void testLock() {
-        //从redis里面获取数据
-        //1.获取当前锁
-        //设置过期时间10s，到时间之后自动释放锁
-        Boolean ifAbsent =
-                redisTemplate.opsForValue()
-                        .setIfAbsent("lock", "lock",10, TimeUnit.SECONDS);
+    public void testLock()  {
 
-        //2.如果获取到锁，从redis获取数据 数据+1 放回redis里面
-        if (ifAbsent){
-            //表示得到当前锁
-            //获取锁成功，执行业务代码
-            //1.先从redis中通过key num获取值  key提前手动设置 num 初始值：0
-            String value = redisTemplate.opsForValue().get("num");
-            //2.如果值为空则非法直接返回即可
-            if (StringUtils.isBlank(value)) {
-                return;
-            }
-            //3.对num值进行自增加一
-            int num = Integer.parseInt(value);
-            redisTemplate.opsForValue().set("num", String.valueOf(++num));
+        //1 通过redisson创建锁对象
+        RLock lock = redissonClient.getLock("lock1");
 
-            //4.释放锁
-            redisTemplate.delete("lock");
-        }else {
-            try {
-                Thread.sleep(100);
-                this.testLock();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+        //2 尝试获取锁
+        //(1) 阻塞一直等待直到获取到，获取锁之后，默认过期时间30s
+        lock.lock();
+
+        //(2) 获取到锁，锁过期时间10s
+       // lock.lock(10,TimeUnit.SECONDS);
+
+        //(3) 第一个参数获取锁等待时间
+        //    第二个参数获取到锁，锁过期时间
+        //        try {
+        //            // true
+        //            boolean tryLock = lock.tryLock(30, 10, TimeUnit.SECONDS);
+        //        } catch (InterruptedException e) {
+        //            throw new RuntimeException(e);
+        //        }
+
+        //3 编写业务代码
+        //1.先从redis中通过key num获取值  key提前手动设置 num 初始值：0
+        String value = redisTemplate.opsForValue().get("num");
+        //2.如果值为空则非法直接返回即可
+        if (StringUtils.isBlank(value)) {
+            return;
         }
+        //3.对num值进行自增加一
+        int num = Integer.parseInt(value);
+        redisTemplate.opsForValue().set("num", String.valueOf(++num));
+
+        //4 释放锁
+        lock.unlock();
     }
+
+
+//    //redis里面setnx实现
+//    @Override
+//    public synchronized void testLock() {
+//        //从redis里面获取数据
+//        //1.获取当前锁
+//        //设置过期时间10s，到时间之后自动释放锁
+//        Boolean ifAbsent =
+//                redisTemplate.opsForValue()
+//                        .setIfAbsent("lock", "lock",10, TimeUnit.SECONDS);
+//
+//        //2.如果获取到锁，从redis获取数据 数据+1 放回redis里面
+//        if (ifAbsent){
+//            //表示得到当前锁
+//            //获取锁成功，执行业务代码
+//            //1.先从redis中通过key num获取值  key提前手动设置 num 初始值：0
+//            String value = redisTemplate.opsForValue().get("num");
+//            //2.如果值为空则非法直接返回即可
+//            if (StringUtils.isBlank(value)) {
+//                return;
+//            }
+//            //3.对num值进行自增加一
+//            int num = Integer.parseInt(value);
+//            redisTemplate.opsForValue().set("num", String.valueOf(++num));
+//
+//            //4.释放锁
+//            redisTemplate.delete("lock");
+//        }else {
+//            try {
+//                Thread.sleep(100);
+//                this.testLock();
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//    }
 
 
     //lua脚本保证原子性
