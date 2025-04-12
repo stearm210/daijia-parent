d2ee8f105255a7891338a38c3c33c6a4554da453
stearm210 <pyzxWoo@163.com>
2025-04-12 15:32:59 +0800

    蹭课段查找当前订单

--- web/web-customer/src/main/java/com/atguigu/daijia/customer/controller/OrderController.java
+++ web/web-customer/src/main/java/com/atguigu/daijia/customer/controller/OrderController.java
@@ -41,14 +41,13 @@ public class OrderController {
       * @Description: 乘客端订单操作
       */
 
-     //TODO 后续完善，目前假设乘客当前没有订单
-     @Operation(summary = "查找乘客端当前订单")
+     //
+     @Operation(summary = "乘客端查找当前订单")
      @GuiguLogin
      @GetMapping("/searchCustomerCurrentOrder")
      public Result<CurrentOrderInfoVo> searchCustomerCurrentOrder() {
-         CurrentOrderInfoVo currentOrderInfoVo = new CurrentOrderInfoVo();
-         currentOrderInfoVo.setIsHasCurrentOrder(false);
-         return Result.ok(currentOrderInfoVo);
+         Long customerId = AuthContextHolder.getUserId();
+         return Result.ok(orderService.searchCustomerCurrentOrder(customerId));
      }
 
 //    @Operation(summary = "乘客端查找当前订单")
