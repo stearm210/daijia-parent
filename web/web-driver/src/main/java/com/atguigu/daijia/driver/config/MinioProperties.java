41111b4629fd1c7b8eec03e9f29a5763e2cfc5ab
stearm210 <pyzxWoo@163.com>
2025-04-19 15:50:49 +0800

    minio值

--- web/web-driver/src/main/java/com/atguigu/daijia/driver/config/MinioProperties.java
+++ web/web-driver/src/main/java/com/atguigu/daijia/driver/config/MinioProperties.java
@@ -4,6 +4,14 @@ import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
+ /*
+  * @Title:
+  * @Author: pyzxW
+  * @Date: 2025-04-19 15:50:39
+  * @Params:
+  * @Return: null
+  * @Description: 创建配置类，读取minio值
+  */
 @Configuration
 @ConfigurationProperties(prefix="minio") //读取节点
 @Data
