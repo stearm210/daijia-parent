051f1cc178309a75a89ba1fefcf440c3c46b34ca
stearm210 <pyzxWoo@163.com>
2025-04-21 15:39:31 +0800

    实际的里程数

--- service/service-map/src/main/java/com/atguigu/daijia/map/service/impl/LocationServiceImpl.java
+++ service/service-map/src/main/java/com/atguigu/daijia/map/service/impl/LocationServiceImpl.java
@@ -400,48 +400,73 @@ public class LocationServiceImpl implements LocationService {
     @Override
     public BigDecimal calculateOrderRealDistance(Long orderId) {
         //1 根据订单id获取代驾订单位置信息，根据创建时间排序（升序）
-        //查询MongoDB
-        //第一种方式
+        //先进行查询，之后将查询的结果进行排序，拍完序之后塞到list集合中
+        //查询MongDB
 //        OrderServiceLocation orderServiceLocation = new OrderServiceLocation();
 //        orderServiceLocation.setOrderId(orderId);
 //        Example<OrderServiceLocation> example = Example.of(orderServiceLocation);
+//
+//        //排序字段
 //        Sort sort = Sort.by(Sort.Direction.ASC, "createTime");
 //        List<OrderServiceLocation> list = orderServiceLocationRepository.findAll(example, sort);
-        //第二种方式
-        //MongoRepository只需要 按照规则 在MongoRepository把查询方法创建出来就可以了
-        // 总体规则：
-        //1 查询方法名称 以 get  |  find  | read开头
-        //2 后面查询字段名称，满足驼峰式命名，比如OrderId
-        //3 字段查询条件添加关键字，比如Like  OrderBy   Asc
-        // 具体编写 ： 根据订单id获取代驾订单位置信息，根据创建时间排序（升序）
-        List<OrderServiceLocation> list =
-                orderServiceLocationRepository.findByOrderIdOrderByCreateTimeAsc(orderId);
 
-        //2 第一步查询返回订单位置信息list集合
-        //把list集合遍历，得到每个位置信息，计算两个位置距离
-        //把计算所有距离相加操作
-        double realDistance = 0;
-        if(!CollectionUtils.isEmpty(list)) {
-            for (int i = 0,size = list.size()-1; i < size; i++) {
-                OrderServiceLocation location1 = list.get(i);
-                OrderServiceLocation location2 = list.get(i + 1);
+        //第二种方式，使用MongoRepository
 
-                //计算位置距离
-                double distance = LocationUtil.getDistance(location1.getLatitude().doubleValue(),
-                        location1.getLongitude().doubleValue(),
-                        location2.getLatitude().doubleValue(),
-                        location2.getLongitude().doubleValue());
 
-                realDistance += distance;
-            }
-        }
+        //2 第一步查询返回订单位置信息List集合
+        //把List集合进行遍历，得到每个位置的信息，计算两个位置的距离
+        //将计算所有的距离进行相加操作
 
-        //TODO 为了测试，不好测试实际代驾距离，模拟数据  实际距离=预估距离+5公里
-        if(realDistance == 0) {
-            return orderInfoFeignClient.getOrderInfo(orderId).getData().getExpectDistance().add(new BigDecimal("5"));
-        }
 
-        //3 返回最终计算实际距离
-        return new BigDecimal(realDistance);
+        //3 返回最终计算的实际距离
+
+        return null;
     }
 }
+//    public BigDecimal calculateOrderRealDistance(Long orderId) {
+//        //1 根据订单id获取代驾订单位置信息，根据创建时间排序（升序）
+//        //查询MongoDB
+//        //第一种方式
+////        OrderServiceLocation orderServiceLocation = new OrderServiceLocation();
+////        orderServiceLocation.setOrderId(orderId);
+////        Example<OrderServiceLocation> example = Example.of(orderServiceLocation);
+////        Sort sort = Sort.by(Sort.Direction.ASC, "createTime");
+////        List<OrderServiceLocation> list = orderServiceLocationRepository.findAll(example, sort);
+//        //第二种方式
+//        //MongoRepository只需要 按照规则 在MongoRepository把查询方法创建出来就可以了
+//        // 总体规则：
+//        //1 查询方法名称 以 get  |  find  | read开头
+//        //2 后面查询字段名称，满足驼峰式命名，比如OrderId
+//        //3 字段查询条件添加关键字，比如Like  OrderBy   Asc
+//        // 具体编写 ： 根据订单id获取代驾订单位置信息，根据创建时间排序（升序）
+//        List<OrderServiceLocation> list =
+//                orderServiceLocationRepository.findByOrderIdOrderByCreateTimeAsc(orderId);
+//
+//        //2 第一步查询返回订单位置信息list集合
+//        //把list集合遍历，得到每个位置信息，计算两个位置距离
+//        //把计算所有距离相加操作
+//        double realDistance = 0;
+//        if(!CollectionUtils.isEmpty(list)) {
+//            for (int i = 0,size = list.size()-1; i < size; i++) {
+//                OrderServiceLocation location1 = list.get(i);
+//                OrderServiceLocation location2 = list.get(i + 1);
+//
+//                //计算位置距离
+//                double distance = LocationUtil.getDistance(location1.getLatitude().doubleValue(),
+//                        location1.getLongitude().doubleValue(),
+//                        location2.getLatitude().doubleValue(),
+//                        location2.getLongitude().doubleValue());
+//
+//                realDistance += distance;
+//            }
+//        }
+//
+//        //TODO 为了测试，不好测试实际代驾距离，模拟数据  实际距离=预估距离+5公里
+//        if(realDistance == 0) {
+//            return orderInfoFeignClient.getOrderInfo(orderId).getData().getExpectDistance().add(new BigDecimal("5"));
+//        }
+//
+//        //3 返回最终计算实际距离
+//        return new BigDecimal(realDistance);
+//    }
+//}
