5b0b584ec895e1c4e184888fa835c0a065305044
stearm210 <pyzxWoo@163.com>
2025-05-10 20:29:48 +0800

    发送账单信息

--- service/service-driver/src/main/java/com/atguigu/daijia/driver/service/impl/CiServiceImpl.java
+++ service/service-driver/src/main/java/com/atguigu/daijia/driver/service/impl/CiServiceImpl.java
@@ -66,59 +66,58 @@ public class CiServiceImpl implements CiService {
       * @Return: null
       * @Description: 文本审核操作
       */
-    @Override
-    public TextAuditingVo textAuditing(String content) {
-        //内容为空，则不需要审核
-        if(!StringUtils.hasText(content)) {
-            TextAuditingVo textAuditingVo = new TextAuditingVo();
-            textAuditingVo.setResult("0");
-            return textAuditingVo;
-        }
+     @Override
+     public TextAuditingVo textAuditing(String content) {
+         if(!StringUtils.hasText(content)) {
+             TextAuditingVo textAuditingVo = new TextAuditingVo();
+             textAuditingVo.setResult("0");
+             return textAuditingVo;
+         }
 
-        COSClient cosClient = this.getCosClient();
+         COSClient cosClient = this.getCosClient();
 
-        //1.创建任务请求对象
-        TextAuditingRequest request = new TextAuditingRequest();
-        //2.添加请求参数 参数详情请见 API 接口文档
-        request.setBucketName(tencentCloudProperties.getBucketPrivate());
-        //2.1.1设置请求内容,文本内容的Base64编码
-        byte[] encoder = org.apache.commons.codec.binary.Base64.encodeBase64(content.getBytes());
-        String contentBase64 = new String(encoder);
-        request.getInput().setContent(contentBase64);
-        request.getConf().setDetectType("all");
+         //1.创建任务请求对象
+         TextAuditingRequest request = new TextAuditingRequest();
+         //2.添加请求参数 参数详情请见 API 接口文档
+         request.setBucketName(tencentCloudProperties.getBucketPrivate());
+         //2.1.1设置请求内容,文本内容的Base64编码
+         byte[] encoder = org.apache.commons.codec.binary.Base64.encodeBase64(content.getBytes());
+         String contentBase64 = new String(encoder);
+         request.getInput().setContent(contentBase64);
+         request.getConf().setDetectType("all");
 
-        //3.调用接口,获取任务响应对象
-        TextAuditingResponse response = cosClient.createAuditingTextJobs(request);
-        AuditingJobsDetail detail = response.getJobsDetail();
-        TextAuditingVo textAuditingVo = new TextAuditingVo();//危险关键字的类
-        if ("Success".equals(detail.getState())) {
-            //检测结果: 0（审核正常），1 （判定为违规敏感文件），2（疑似敏感，建议人工复核）。
-            String result = detail.getResult();
+         //3.调用接口,获取任务响应对象
+         TextAuditingResponse response = cosClient.createAuditingTextJobs(request);
+         AuditingJobsDetail detail = response.getJobsDetail();
+         TextAuditingVo textAuditingVo = new TextAuditingVo();
+         if ("Success".equals(detail.getState())) {
+             //检测结果: 0（审核正常），1 （判定为违规敏感文件），2（疑似敏感，建议人工复核）。
+             String result = detail.getResult();
 
-            //违规关键词
-            StringBuffer keywords = new StringBuffer();
-            List<SectionInfo> sectionInfoList = detail.getSectionList();
-            for (SectionInfo info : sectionInfoList) {
+             //违规关键词
+             StringBuffer keywords = new StringBuffer();
+             List<SectionInfo> sectionInfoList = detail.getSectionList();
+             for (SectionInfo info : sectionInfoList) {
 
-                String pornInfoKeyword = info.getPornInfo().getKeywords();
-                String illegalInfoKeyword = info.getIllegalInfo().getKeywords();
-                String abuseInfoKeyword = info.getAbuseInfo().getKeywords();
+                 String pornInfoKeyword = info.getPornInfo().getKeywords();
+                 String illegalInfoKeyword = info.getIllegalInfo().getKeywords();
+                 String abuseInfoKeyword = info.getAbuseInfo().getKeywords();
 
-                if (pornInfoKeyword.length() > 0) {
-                    keywords.append(pornInfoKeyword).append(",");
-                }
-                if (illegalInfoKeyword.length() > 0) {
-                    keywords.append(illegalInfoKeyword).append(",");
-                }
-                if (abuseInfoKeyword.length() > 0) {
-                    keywords.append(abuseInfoKeyword).append(",");
-                }
-            }
-            textAuditingVo.setResult(result);
-            textAuditingVo.setKeywords(keywords.toString());
-        }
-        return textAuditingVo;
-    }
+                 if (pornInfoKeyword.length() > 0) {
+                     keywords.append(pornInfoKeyword).append(",");
+                 }
+                 if (illegalInfoKeyword.length() > 0) {
+                     keywords.append(illegalInfoKeyword).append(",");
+                 }
+                 if (abuseInfoKeyword.length() > 0) {
+                     keywords.append(abuseInfoKeyword).append(",");
+                 }
+             }
+             textAuditingVo.setResult(result);
+             textAuditingVo.setKeywords(keywords.toString());
+         }
+         return textAuditingVo;
+     }
 
     public COSClient getCosClient() {
         String secretId = tencentCloudProperties.getSecretId();
